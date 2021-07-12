6d6e8d86b92b4f9549209f34b9cc5009620f1d61
ciprianhotea <cipryy_13@yahoo.com>
2021-07-12 15:50:39 +0300

    Conventions completed.

--- Conventions/Comparison/src/Task.kt
+++ Conventions/Comparison/src/Task.kt
@@ -1,5 +1,13 @@
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-    /* TODO */
+    override fun compareTo(other: MyDate): Int {
+        if(year > other.year) return 1
+        else if(year < other.year) return -1;
+        else if(month > other.month) return 1;
+        else if(month < other.month) return -1;
+        else if(dayOfMonth > other.dayOfMonth) return 1;
+        else if(dayOfMonth < other.dayOfMonth) return -1;
+        else return 0;
+    }
 }
 
 fun test(date1: MyDate, date2: MyDate) {
